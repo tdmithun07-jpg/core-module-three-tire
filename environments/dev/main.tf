b73b945c4482b75b872f6c82f3e7d4fc261de1fc
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-23 18:51:37 +0530

    2312 nsg nic pub-ip vms added 5

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,6 +5,7 @@ module "resource_group" {
 module "network" {
   source              = "github.com/tdmithun07-jpg/core-module-three-tire/modules/networking"
   resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.location
 }
 
 module "compute" {
